668f8a728c6ae8502fb649210d10cf68f411a80b
ishandhanani <ishandhanani@gmail.com>
2024-07-06 14:16:14 -0700

    added op

--- engine/engine.go
+++ engine/engine.go
@@ -3,18 +3,19 @@ package engine
 type Value struct {
 	Data float64
 	Prev []*Tuple
+	Op   string
 }
 
-func NewValue(data float64, prev []*Tuple) *Value {
-	return &Value{data, prev}
+func NewValue(data float64, prev []*Tuple, op string) *Value {
+	return &Value{data, prev, op}
 }
 
 func (v *Value) Add(v2 *Value) *Value {
 	v.Prev = append(v.Prev, &Tuple{v, v2})
-	return &Value{Data: v.Data + v2.Data, Prev: []*Tuple{{v, v2}}}
+	return &Value{Data: v.Data + v2.Data, Prev: []*Tuple{{v, v2}}, Op: "+"}
 }
 
 func (v *Value) Multiply(v2 *Value) *Value {
 	v.Prev = append(v.Prev, &Tuple{v, v2})
-	return &Value{Data: v.Data * v2.Data, Prev: []*Tuple{{v, v2}}}
+	return &Value{Data: v.Data * v2.Data, Prev: []*Tuple{{v, v2}}, Op: "*"}
 }
