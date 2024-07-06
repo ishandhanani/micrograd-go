b653428f3f676e8715df160f41461491ab853495
ishandhanani <ishandhanani@gmail.com>
2024-07-06 13:05:21 -0700

    added children and prev field

--- engine/engine.go
+++ engine/engine.go
@@ -1,13 +1,22 @@
 package engine
 
+type children struct {
+	current *Value // in the video this is self
+	other   *Value // in the video this is other
+}
 type Value struct {
 	Data float64
+	prev []*children // in the video this is a set of children tuples
+}
+
+func NewValue(data float64, prev []*children) *Value {
+	return &Value{data, prev}
 }
 
 func (v *Value) Add(v2 *Value) *Value {
-	return &Value{v.Data + v2.Data}
+	return &Value{Data: v.Data + v2.Data, prev: []*children{{current: v, other: v2}}}
 }
 
 func (v *Value) Multiply(v2 *Value) *Value {
-	return &Value{v.Data * v2.Data}
+	return &Value{Data: v.Data * v2.Data, prev: []*children{{current: v, other: v2}}}
 }
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ishandhanani/micrograd-go/engine"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAdd(t *testing.T) {
@@ -22,4 +23,14 @@ func TestMultiply(t *testing.T) {
 	if v3.Data != 2 {
 		t.Errorf("Expected %v, got %v", 2, v3.Data)
 	}
+}
+
+func TestVideoExample1(t *testing.T) {
+	a := engine.Value{Data: 2.0}
+	b := engine.Value{Data: -3.0}
+	c := engine.Value{Data: 10.0}
+
+	res := a.Multiply(&b).Add(&c)
+
+	assert.Equal(t, res.Data, 4.0)
 }
